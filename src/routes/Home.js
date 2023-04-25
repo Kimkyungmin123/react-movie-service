f60fdd075cbc70e8e32de72f9d34b8c6b4e8449c
Kimkyungmin123 <rud3302@naver.com>
2023-04-26 06:11:54 +0900

    chore: remove console.log

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,51 +1,49 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 import styles from "./Home.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner  } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-function Home(){
-    const [loading, setLoading] =useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async()=>{
-        const json= await(
-            await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-            )).json();
-            setMovies(json.data.movies);
-            setLoading(false);
-    };
-    console.log(movies);
-    useEffect(()=>{
-        getMovies();
-    },[])
-return(
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
     <div className={styles.container}>
-        {loading ? (
+      {loading ? (
         <div className={styles.loader}>
-            <span>loading... </span>
-            
-            <FontAwesomeIcon
-                                icon={faSpinner} 
-                                className={styles.icon}
-                                />
-            </div> 
-        ): (
-            <div className={styles.movies}>
-                {movies.map((movie)=>(
-                    <Movie
-                    id={movie.id}
-                    key={movie.id}
-                    year={movie.year}
-                    coverImg={movie.medium_cover_image}
-                    title={movie.title}
-                    genres={movie.genres}
-                    synopsis={movie.synopsis}
-                    />
-                ))}
-            </div>
-        )}
+          <span>loading... </span>
+
+          <FontAwesomeIcon icon={faSpinner} className={styles.icon} />
+        </div>
+      ) : (
+        <div className={styles.movies}>
+          {movies.map((movie) => (
+            <Movie
+              id={movie.id}
+              key={movie.id}
+              year={movie.year}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              genres={movie.genres}
+              synopsis={movie.synopsis}
+            />
+          ))}
+        </div>
+      )}
     </div>
-)
+  );
 }
 
 export default Home;
