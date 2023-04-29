8e5a669a6c7da1012698f40b11d90dc6bcfcd762
Kimkyungmin123 <rud3302@naver.com>
2023-04-29 15:37:43 +0900

    feat: set react-query

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+          <Route path="/movie/:id" element={<Detail />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
