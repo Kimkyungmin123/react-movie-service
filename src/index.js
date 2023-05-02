23c1b636e13389490d7204e3217d168e9970eda8
Kimkyungmin123 <rud3302@naver.com>
2023-05-02 12:06:33 +0900

    form: remove space

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import App from "./App";
 import "./styles.css";
-
 import { createRoot } from "react-dom/client";
+
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />);
